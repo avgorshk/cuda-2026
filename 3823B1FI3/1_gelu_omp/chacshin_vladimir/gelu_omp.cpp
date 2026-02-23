9b10f84c9fb60e10cc89ab5986f25015d2d9e869
kodt2 <kodt2005@yandex.ru>
2026-02-24 01:03:40 +0300

    this version compute in 2.68e-7 prec

--- 3823B1FI3/1_gelu_omp/chacshin_vladimir/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/chacshin_vladimir/gelu_omp.cpp
@@ -15,6 +15,9 @@ inline __m256 exp256_ps_high_prec(__m256 x) {
     const __m256 c2 = _mm256_set1_ps(1.0f / 6.0f);
     const __m256 c3 = _mm256_set1_ps(1.0f / 24.0f);
     const __m256 c4 = _mm256_set1_ps(1.0f / 120.0f);
+    const __m256 c5 = _mm256_set1_ps(1.0f / 720.0f);
+    const __m256 c6 = _mm256_set1_ps(1.0f / 5040.0f);
+    const __m256 c7 = _mm256_set1_ps(1.0f / 40320.0f);
     const __m256 one = _mm256_set1_ps(1.0f);
 
 
@@ -29,20 +32,27 @@ inline __m256 exp256_ps_high_prec(__m256 x) {
     __m256 f3 = _mm256_mul_ps(f2, f);
     __m256 f4 = _mm256_mul_ps(f3, f);
     __m256 f5 = _mm256_mul_ps(f4, f);
+    __m256 f6 = _mm256_mul_ps(f5, f);
+    __m256 f7 = _mm256_mul_ps(f6, f);
+    __m256 f8 = _mm256_mul_ps(f7, f);
 
     __m256 poly = _mm256_add_ps(one,
         _mm256_add_ps(f,
             _mm256_add_ps(_mm256_mul_ps(f2, c1),
                 _mm256_add_ps(_mm256_mul_ps(f3, c2),
                     _mm256_add_ps(_mm256_mul_ps(f4, c3),
-                        _mm256_mul_ps(f5, c4))))));
+                        _mm256_add_ps(_mm256_mul_ps(f5, c4),
+                            _mm256_add_ps(_mm256_mul_ps(f6, c5),
+                                _mm256_add_ps(_mm256_mul_ps(f7, c6),
+                                    _mm256_mul_ps(f8, c7)
+                                ))))))));
 
 
-                        __m256i e = _mm256_add_epi32(n, _mm256_set1_epi32(127));
-                        e = _mm256_slli_epi32(e, 23);
-                        __m256 pow2n = _mm256_castsi256_ps(e);
+    __m256i e = _mm256_add_epi32(n, _mm256_set1_epi32(127));
+    e = _mm256_slli_epi32(e, 23);
+    __m256 pow2n = _mm256_castsi256_ps(e);
 
-                        return _mm256_mul_ps(poly, pow2n);
+    return _mm256_mul_ps(poly, pow2n);
 }
 
 std::vector<float> GeluOMP(const std::vector<float>& input) {
