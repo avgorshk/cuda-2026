867f9954a79edb686f8c64bc7db0a7b71268bbdf
Andrew-8705 <abaldin05@mail.ru>
2026-02-25 17:59:19 +0300

    3_naive_gemm_omp

---
+++ 3823B1FI3/3_naive_gemm_omp/baldin_andrew/naive_gemm_omp.cpp
@@ -0,0 +1,25 @@
+#pragma GCC optimize("Ofast, unroll-loops")
+#pragma GCC target("avx2,fma")
+
+#include "naive_gemm_omp.h"
+#include <omp.h>
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n) {
+
+    std::vector<float> c(n * n, 0.0f);
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) {
+            float tmp = a[i * n + k];
+            #pragma omp simd
+            for (int j = 0; j < n; j++) {
+                c[i * n + j] += tmp * b[k * n + j];
+            }
+        }
+    }
+    
+    return c;
+}
