b6b76b20c66339368b3ef3e1ccc8fc54c8591c14
unknown <the_h1nk@mail.ru>
2026-03-01 10:02:07 +0300

    kinda do first task

--- 3823B1FI3/1_gelu_omp/zavyalov_alexey/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/zavyalov_alexey/gelu_omp.cpp
@@ -1,5 +1,29 @@
 #include "gelu_omp.h"
 
 std::vector<float> GeluOMP(const std::vector<float>& input) {
-    // Place your implementation here
+    std::vector<float> res(input.size());
+    int n = input.size();
+#pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        float x = input[i];
+        float sqrt_2_div_pi = 0.797884560802865355879892f;
+        float tanh_arg = sqrt_2_div_pi * (x + 0.044715 * x * x * x);
+        float exp_x = exp(tanh_arg);
+        float exp_minus_x = exp(-tanh_arg);
+        float tanh_cherez_exp = (exp_x - exp_minus_x) / (exp_x + exp_minus_x);
+        res[i] = 0.5 * x * (1 + tanh_cherez_exp);
+    }
+    return res;
+}
+
+std::vector<float> GeluOMP_slow(const std::vector<float>& input) {
+    std::vector<float> res(input.size());
+    int n = input.size();
+    // #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        float x = input[i];
+
+        res[i] = 0.5 * x * (1 + tanh(0.797884560802865355879892f * (x + 0.044715 * x * x * x)));
+    }
+    return res;
 }
--- 3823B1FI3/1_gelu_omp/zavyalov_alexey/gelu_omp.h
+++ 3823B1FI3/1_gelu_omp/zavyalov_alexey/gelu_omp.h
@@ -2,7 +2,10 @@
 #define __GELU_OMP_H
 
 #include <vector>
+#include <cmath>
+
 
 std::vector<float> GeluOMP(const std::vector<float>& input);
+std::vector<float> GeluOMP_slow(const std::vector<float>& input);
 
 #endif // __GELU_OMP_H
